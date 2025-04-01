3c290c1d250223309997e50487b51991adc57e2d
ychandupa1999 <yashasvic@icloud.com>
2025-04-01 16:11:05 +0530

    Project setup

---
+++ CamSync/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace CamSync
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            cboCamera = new ComboBox();
+            cboCamera2 = new ComboBox();
+            pic = new PictureBox();
+            pic2 = new PictureBox();
+            btnStart = new Button();
+            btnStop1 = new Button();
+            btnStop2 = new Button();
+            btnStart2 = new Button();
+            btnInfo = new Button();
+            linePanel = new Panel();
+            linePanelLeft = new Panel();
+            linePanelRight = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(67, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Select Left Camera :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(586, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Select Right Camera :";
+            // 
+            // cboCamera
+            // 
+            cboCamera.FormattingEnabled = true;
+            cboCamera.Location = new Point(184, 72);
+            cboCamera.Name = "cboCamera";
+            cboCamera.Size = new Size(361, 23);
+            cboCamera.TabIndex = 2;
+            // 
+            // cboCamera2
+            // 
+            cboCamera2.FormattingEnabled = true;
+            cboCamera2.Location = new Point(714, 72);
+            cboCamera2.Name = "cboCamera2";
+            cboCamera2.Size = new Size(361, 23);
+            cboCamera2.TabIndex = 3;
+            // 
+            // pic
+            // 
+            pic.BorderStyle = BorderStyle.FixedSingle;
+            pic.Location = new Point(67, 159);
+            pic.Name = "pic";
+            pic.Size = new Size(519, 464);
+            pic.TabIndex = 4;
+            pic.TabStop = false;
+            // 
+            // pic2
+            // 
+            pic2.BorderStyle = BorderStyle.FixedSingle;
+            pic2.Location = new Point(586, 159);
+            pic2.Name = "pic2";
+            pic2.Size = new Size(519, 464);
+            pic2.TabIndex = 5;
+            pic2.TabStop = false;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(470, 113);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 6;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnStop1
+            // 
+            btnStop1.Location = new Point(389, 113);
+            btnStop1.Name = "btnStop1";
+            btnStop1.Size = new Size(75, 23);
+            btnStop1.TabIndex = 7;
+            btnStop1.Text = "Stop";
+            btnStop1.UseVisualStyleBackColor = true;
+            btnStop1.Click += btnStop1_Click;
+            // 
+            // btnStop2
+            // 
+            btnStop2.Location = new Point(919, 113);
+            btnStop2.Name = "btnStop2";
+            btnStop2.Size = new Size(75, 23);
+            btnStop2.TabIndex = 9;
+            btnStop2.Text = "Stop";
+            btnStop2.UseVisualStyleBackColor = true;
+            btnStop2.Click += btnStop2_Click_1;
+            // 
+            // btnStart2
+            // 
+            btnStart2.Location = new Point(1000, 113);
+            btnStart2.Name = "btnStart2";
+            btnStart2.Size = new Size(75, 23);
+            btnStart2.TabIndex = 8;
+            btnStart2.Text = "Start";
+            btnStart2.UseVisualStyleBackColor = true;
+            btnStart2.Click += btnStart2_Click;
+            // 
+            // btnInfo
+            // 
+            btnInfo.BackgroundImage = (Image)resources.GetObject("btnInfo.BackgroundImage");
+            btnInfo.BackgroundImageLayout = ImageLayout.Stretch;
+            btnInfo.FlatAppearance.BorderSize = 0;
+            btnInfo.FlatStyle = FlatStyle.Flat;
+            btnInfo.Location = new Point(1132, 11);
+            btnInfo.Name = "btnInfo";
+            btnInfo.Size = new Size(27, 26);
+            btnInfo.TabIndex = 10;
+            btnInfo.UseVisualStyleBackColor = true;
+            btnInfo.Click += btnInfo_Click_1;
+            // 
+            // linePanel
+            // 
+            linePanel.BackColor = Color.Red;
+            linePanel.Location = new Point(58, 201);
+            linePanel.Name = "linePanel";
+            linePanel.Size = new Size(1056, 2);
+            linePanel.TabIndex = 11;
+            // 
+            // linePanelLeft
+            // 
+            linePanelLeft.BackColor = SystemColors.MenuHighlight;
+            linePanelLeft.Location = new Point(317, 149);
+            linePanelLeft.Name = "linePanelLeft";
+            linePanelLeft.Size = new Size(2, 483);
+            linePanelLeft.TabIndex = 13;
+            // 
+            // linePanelRight
+            // 
+            linePanelRight.BackColor = SystemColors.MenuHighlight;
+            linePanelRight.Location = new Point(820, 149);
+            linePanelRight.Name = "linePanelRight";
+            linePanelRight.Size = new Size(2, 483);
+            linePanelRight.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1172, 688);
+            Controls.Add(linePanelRight);
+            Controls.Add(linePanelLeft);
+            Controls.Add(linePanel);
+            Controls.Add(btnInfo);
+            Controls.Add(btnStop2);
+            Controls.Add(btnStart2);
+            Controls.Add(btnStop1);
+            Controls.Add(btnStart);
+            Controls.Add(pic2);
+            Controls.Add(pic);
+            Controls.Add(cboCamera2);
+            Controls.Add(cboCamera);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CamSync Viewer";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pic).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox cboCamera;
+        private ComboBox cboCamera2;
+        private PictureBox pic;
+        private PictureBox pic2;
+        private Button btnStart;
+        private Button btnStop1;
+        private Button btnStop2;
+        private Button btnStart2;
+        private Button btnInfo;
+        private Panel linePanel;
+        private Panel linePanelLeft;
+        private Panel linePanelRight;
+    }
+}
---
+++ CamSync/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+
+namespace CamSync
+{
+    public partial class Form1 : Form
+    {
+
+        private bool isDraggingHorizontal = false;
+        private int mouseY;
+
+        private bool isDraggingLeft = false;
+        private bool isDraggingRight = false;
+        private int mouseX;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Horizontal line events
+            linePanel.MouseDown += LinePanel_MouseDown;
+            linePanel.MouseMove += LinePanel_MouseMove;
+            linePanel.MouseUp += LinePanel_MouseUp;
+
+            // Left vertical line events
+            linePanelLeft.MouseDown += LinePanelLeft_MouseDown;
+            linePanelLeft.MouseMove += LinePanelLeft_MouseMove;
+            linePanelLeft.MouseUp += LinePanelLeft_MouseUp;
+
+            // Right vertical line events
+            linePanelRight.MouseDown += LinePanelRight_MouseDown;
+            linePanelRight.MouseMove += LinePanelRight_MouseMove;
+            linePanelRight.MouseUp += LinePanelRight_MouseUp;
+        }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice1, videoCaptureDevice2;
+
+
+
+
+
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice1 != null && videoCaptureDevice1.IsRunning)
+            {
+                videoCaptureDevice1.SignalToStop();
+                videoCaptureDevice1.WaitForStop();
+            }
+
+            videoCaptureDevice1 = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice1.NewFrame += VideoCaptureDevice1_NewFrame;
+            videoCaptureDevice1.Start();
+        }
+
+        private void btnStart2_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice2 != null && videoCaptureDevice2.IsRunning)
+            {
+                videoCaptureDevice2.SignalToStop();
+                videoCaptureDevice2.WaitForStop();
+            }
+
+            videoCaptureDevice2 = new VideoCaptureDevice(filterInfoCollection[cboCamera2.SelectedIndex].MonikerString);
+            videoCaptureDevice2.NewFrame += VideoCaptureDevice2_NewFrame;
+            videoCaptureDevice2.Start();
+        }
+
+
+
+
+
+
+
+
+
+        private void VideoCaptureDevice1_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap frame = (Bitmap)eventArgs.Frame.Clone();
+            frame.RotateFlip(RotateFlipType.RotateNoneFlipX); // Flip image horizontally
+            pic.Image = frame;
+        }
+
+        private void VideoCaptureDevice2_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap frame = (Bitmap)eventArgs.Frame.Clone();
+            frame.RotateFlip(RotateFlipType.RotateNoneFlipX); // Flip image horizontally
+            pic2.Image = frame;
+        }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+            {
+                cboCamera.Items.Add(filterInfo.Name);
+                cboCamera2.Items.Add(filterInfo.Name);
+            }
+
+            if (cboCamera.Items.Count > 0)
+                cboCamera.SelectedIndex = 0;
+
+            if (cboCamera2.Items.Count > 0)
+                cboCamera2.SelectedIndex = 0;
+
+            videoCaptureDevice1 = new VideoCaptureDevice();
+            videoCaptureDevice2 = new VideoCaptureDevice();
+        }
+
+
+
+
+
+
+
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice1 != null && videoCaptureDevice1.IsRunning)
+                videoCaptureDevice1.SignalToStop();
+            videoCaptureDevice1.WaitForStop();
+
+            if (videoCaptureDevice2 != null && videoCaptureDevice2.IsRunning)
+                videoCaptureDevice2.SignalToStop();
+            videoCaptureDevice2.WaitForStop();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Horizontal line dragging (Up & Down)
+        private void LinePanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDraggingHorizontal = true;
+            mouseY = e.Y;
+        }
+
+        private void LinePanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingHorizontal)
+            {
+                int newY = linePanel.Top + (e.Y - mouseY);
+
+                // Horizontal line dragging
+                // Set limits for movement (adjust minY and maxY as needed)
+                int minY = 156;  // Minimum Y position
+                int maxY = 625; // Maximum Y position
+
+                if (newY >= minY && newY <= maxY)
+                {
+                    linePanel.Top = newY;
+                }
+            }
+        }
+        private void LinePanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDraggingHorizontal = false;
+        }
+
+        // Left vertical line dragging (Left & Right)
+        private void LinePanelLeft_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDraggingLeft = true;
+            mouseX = e.X;
+        }
+
+        private void LinePanelLeft_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingLeft)
+            {
+                int newX = linePanelLeft.Left + (e.X - mouseX);
+
+                // Left vertical line dragging
+                // Set limits for movement (adjust minX and maxX as needed)
+                int minX = 64;  // Minimum X position
+                int maxX = 584; // Maximum X position
+
+                if (newX >= minX && newX <= maxX)
+                {
+                    linePanelLeft.Left = newX;
+                }
+            }
+        }
+        private void LinePanelLeft_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDraggingLeft = false;
+        }
+
+        // Right vertical line dragging (Left & Right)
+        private void LinePanelRight_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDraggingRight = true;
+            mouseX = e.X;
+        }
+        private void LinePanelRight_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingRight)
+            {
+                int newX = linePanelRight.Left + (e.X - mouseX);
+
+                // Right vertical line dragging
+                // Set limits for movement (adjust minX and maxX as needed)
+                int minX = 585;   // Minimum X position
+                int maxX = 1106;  // Maximum X position
+
+                if (newX >= minX && newX <= maxX)
+                {
+                    linePanelRight.Left = newX;
+                }
+            }
+        }
+
+        private void LinePanelRight_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDraggingRight = false;
+        }
+
+
+
+
+
+
+
+
+
+        private void btnStop1_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice1 != null && videoCaptureDevice1.IsRunning)
+            {
+                videoCaptureDevice1.SignalToStop();
+                videoCaptureDevice1.WaitForStop();
+                pic.Image = null;  // Clear the PictureBox
+            }
+        }
+        private void btnStop2_Click_1(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice2 != null && videoCaptureDevice2.IsRunning)
+            {
+                videoCaptureDevice2.SignalToStop();
+                videoCaptureDevice2.WaitForStop();
+                pic2.Image = null;  // Clear the PictureBox
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        private void btnInfo_Click_1(object sender, EventArgs e)
+        {
+            string aboutText = "DualCam Viewer\n\n" +
+                       "Version: 1.0\n" +
+                       "Developed by: Chandupa Perera\n\n" +
+                       "This application allows you to view and manage two cameras simultaneously.\n" +
+                       "Features:\n" +
+                       "✔ Start & Stop each camera separately\n" +
+                       "✔ Move adjustable guide lines\n" +
+                       "✔ Simple & user-friendly interface\n\n" +
+
+                       "📧 Contact: yashasvichandupa@gmail.com\n" +
+                       "📞 Phone: +94 713684090";
+
+
+            MessageBox.Show(aboutText, "About CamSync Viewer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        
+    }
+}
